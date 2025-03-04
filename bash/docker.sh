f5ea3766bfe654205038cbb167ed511ad0d9026f
Lazycat2017 <congtoukaisi@gmail.com>
2025-03-04 21:06:12 +0800

    new docker install

---
+++ bash/docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# 安装 Docker
+curl -fsSL https://get.docker.com | sh
+systemctl start docker
+systemctl enable docker
+
+# 安装 Docker Compose
+LATEST_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r .tag_name)
+curl -L "https://github.com/docker/compose/releases/download/${LATEST_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose

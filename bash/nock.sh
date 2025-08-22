c15fbfb2579824d5a5219cb0816cc19028931026
Lazycat2017 <congtoukaisi@gmail.com>
2025-08-22 13:20:57 +0800

    fix

--- bash/nock.sh
+++ bash/nock.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# 创建目录(如果不存在)
-mkdir -p /data/nock
-
 # 下载并覆盖文件
-wget https://dl.ka.dog/nock/h9-miner-nock-linux-amd64 -O /data/nock/h9-miner-nock-linux-amd64
+wget https://dl.zao.re/nock/h9-miner-nock-linux-amd64 -O /data/nock/h9-miner-nock-linux-amd64
 
 # 添加执行权限
 chmod +x /data/nock/h9-miner-nock-linux-amd64
